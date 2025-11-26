9686c629203695e791b04bb2da3f6944d6996ca9
MrDJBaumann <dennjbaumann@gmail.com>
2025-11-26 09:10:24 -0800

    added multiplication table

---
+++ Users/Dennis/Documents/Beginner friendly projects/multiplication_table.py
@@ -0,0 +1,8 @@
+print("Multiplication Table Generator")
+print("=" * 30)
+number = int(input("Enter a number: "))
+print("=" * 30) 
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} ={result}")
